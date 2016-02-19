4faa54de51185fc03796e76b2b2e1db546a91be1
dpru <duhsokola@gmail.com>
2016-02-19 14:12:42 +0100

    fixes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use(express.static(__dirname + '/www'));
+app.use(express.static(__dirname + '/src'));
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
